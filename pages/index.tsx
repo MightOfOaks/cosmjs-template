09efc11e55cf926987ee1ebb3b4a55c1ef6c7e8d
Serkan Reis <serkanreis@gmail.com>
2022-11-30 10:01:26 +0300

    Test UI alternatives

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,6 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 import {
   CosmWasmClient,
   SigningCosmWasmClient,
@@ -11,13 +14,24 @@ import { getSigner } from "../wallet";
 import { coin } from "@cosmjs/amino";
 import { Timestamp } from "cosmjs-types/google/protobuf/timestamp";
 import { Coin } from "cosmjs-types/cosmos/base/v1beta1/coin";
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
 
 const IS_TESTNET = !process.argv.includes("--mainnet");
 
 const MAINNET_RPC = "https://rpc.juno-1.deuslabs.fi";
 const TESTNET_RPC = "https://rpc.streamswap-devnet.omniflix.co:443/";
+
+let client: SigningCosmWasmClient
+
+let subscribe = (person:any) => {
+  let signer = getSigner(person.mnemonic).then((signer) => {let client =SigningCosmWasmClient.connectWithSigner(
+    TESTNET_RPC,
+    signer,
+    {
+      prefix: "wasm",
+      gasPrice: GasPrice.fromString("0.025uwasm"),
+    }
+  )}).then((client) => {console.log(client)})
+  }
 
 let Bob = {
   address: "wasm1lwcrr9ktsmn2f7fej6gywxcm8uvxlzz5ch40hm",
@@ -47,124 +61,32 @@ let Rick = {
   at_time: 1 / 2,
 };
 
-const Home: NextPage = () => {
-const [streamInfo, setStreamInfo] = useState("");
 
-  const CONTRACT_ADDRESS =
-  "wasm1sr06m8yqg0wzqqyqvzvp5t07dj4nevx9u8qc7j4qa72qu8e3ct8qzuktnp"
-
-  let signerBob, signerAlice, signerRick : DirectSecp256k1HdWallet
-  let clientBob: SigningCosmWasmClient
-  let clientAlice: SigningCosmWasmClient
-  let clientRick: SigningCosmWasmClient
-  
-  const init = async () => {
-    signerBob = await getSigner(Bob.mnemonic);
-    signerAlice = await getSigner(Alice.mnemonic);
-    signerRick = await getSigner(Rick.mnemonic);
-    
-    clientBob = await SigningCosmWasmClient.connectWithSigner(
-    IS_TESTNET ? TESTNET_RPC : MAINNET_RPC,
-    signerBob,
-    {
-      prefix: "wasm",
-      gasPrice: GasPrice.fromString("0.025uwasm"),
-    }
-  )
-
-  clientAlice = await SigningCosmWasmClient.connectWithSigner(
-    IS_TESTNET ? TESTNET_RPC : MAINNET_RPC,
-    signerAlice,
-    {
-      prefix: "wasm",
-      gasPrice: GasPrice.fromString("0.025uwasm"),
-    }
-  )
-
-  clientRick = await SigningCosmWasmClient.connectWithSigner(
-    IS_TESTNET ? TESTNET_RPC : MAINNET_RPC,
-    signerRick,
-    {
-      prefix: "wasm",
-      gasPrice: GasPrice.fromString("0.025uwasm"),
-    }
-  )
-  }
-
-  useEffect (() => {
-    init()
-  }, [])
-
-  const updateDistribution = async () => {
-    const response = await clientBob.execute(Bob.address,
-      CONTRACT_ADDRESS,
-      {
-        update_distribution: {
-          stream_id: 1,
-        },
-      },
-      "auto",
-    )
-    console.log(response)
-  }
-
-  const queryStream = async () => {
-    const response = await clientBob.queryContractSmart(
-      CONTRACT_ADDRESS,
-    {
-      stream: { stream_id: 1 },
-    })
-    console.log(response)
-    setStreamInfo(JSON.stringify(response))
-  }
-
-  const queryPositionBob = async () => {
-    const response = await clientBob.queryContractSmart(
-      CONTRACT_ADDRESS,
-    {
-      position: { stream_id: 1, owner: Bob.address },
-    })
-    console.log(response)
-  }
-
-  const subscribeBob = async () => {
-    const response = await clientBob.execute(Bob.address,
-      CONTRACT_ADDRESS,
-      {
-        subscribe: {
-          stream_id: 1,
-          position_owner: null,
-          operator: null
-        },
-      },
-      "auto",
-    )
-    console.log(response)
-  }
-
-
+export default function Home() {
   return (
-    <div className='ml-9 mt-40  flex flex-col'>
-      <div className="flex flex-col mb-10">
-        <input className='w-full h-48 border-2 border-black overflow-scroll overflow-x-auto' type="text" value={streamInfo}/>
-        <div className="flex flex-row">
-          <button className="w-[100px] border-2 rounded-sm" onClick={queryStream}>Query Stream</button>
-          <button className="w-[100px] border-2 rounded-sm" onClick={updateDistribution}>Update Distribution</button>
-         </div>
-      </div>
-      <input type={'text'} defaultValue={JSON.stringify(Rick)} className='w-full h-48 border-2 border-black overflow-scroll overflow-x-auto'/>
-      <div className="flex flex-row mb-8">
-        <button className="w-[100px] border-2 rounded-sm" onClick={queryPositionBob}>Query Bob's Position</button>
-        <button className="w-[100px] border-2 rounded-sm" onClick={subscribeBob}>Subscribe for Bob</button>
-        {/* <button title="Withdraw" onClick={withdrawBob}/>
-        <button title="Exit Stream" onClick={exitBob}/> */}
-      </div>
-      <input type={'text'} defaultValue="adssd" className='w-1/3 h-48 border-2 border-black'/>
-      <input type={'text'} defaultValue="adssd" className='w-1/3 h-48 border-2 border-black'/>
-      <input type={'text'} defaultValue="adssd" className='w-1/3 h-48 border-2 border-black'/>
+    <div className='ml-9 mt-40  flex-auto '>
+     <span>
+      <h1 className='h-10 font-bold'>Rick</h1>
+      {JSON.stringify(Rick)}</span>
+      <button onClick={() => subscribe(Rick)}>
+        subscribe to stream 
+      </button>
+     <span>
+     <h1 className='h-10 font-bold'>Alice</h1>
+     {JSON.stringify(Alice)}
+     <button onClick={subscribe}>
+        subscribe to stream 
+      </button>
+     </span>
+     <span><h1 className='h-10 font-bold'>Bob</h1>
+      {JSON.stringify(Bob)}
+      <button onClick={subscribe}>
+        subscribe to stream 
+      </button>
+      </span>
+
 
     </div>
 
   )
 }
-export default Home
